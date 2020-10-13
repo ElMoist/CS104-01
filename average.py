1da903d21a43ea76feefc7c582062153f79da408
ElMoist <71726131+ElMoist@users.noreply.github.com>
2020-10-13 16:08:02 -0400

    Add files via upload

--- average.py
+++ average.py
@@ -1,5 +1,19 @@
-numberOfTests = int(input("Please enter the number of tests you want to average: "))
-score1 = int(input("Please enter a score: "))
-score2 = int(input("Please enter a score: "))
-average = (score1 + score2)/2
-print("The average is ",average)
+numberOfTests = 0
+score = 0
+total = 0
+average = 0.0
+scoreCount = 0
+
+
+
+numberOfTests=int(input("Please enter the number of tests you want to average: ") )
+
+while scoreCount < numberOfTests:
+    score=int(input("Please enter a score: ") )
+    scoreCount= scoreCount + 1
+    total= total+score
+    
+
+
+average=total/scoreCount
+print("The average is: ",average)
